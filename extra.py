6045ce8b3a88ce1160e78145eba877c4b05c81f2
metrokot <maksimsmir94@gmail.com>
2023-04-27 22:12:17 +0300

    how to draw function of 2 params with value

--- extra.py
+++ extra.py
@@ -1,14 +1,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
-fig = plt.figure()
-ax = fig.add_subplot(1,1,1)
 x1,x1x2,x2,x0,e,M=1,0.5,5,(0,0.5),(0.15,0.2),10
-#x1,x1x2,x2,x0,e,M=2,1,1,(0.5,1),(0.1,0.15),10
-sns.set_theme(style="whitegrid")
-
-x, y = np.meshgrid(np.linspace(-5, 5, 100), np.linspace(-5, 5, 100))
-z = x1 * (x ** 2) + x1x2 *x*y + (y**2) * x2 -9
-ax.contour(x, y, (x1 * (x ** 2) + x1x2 *x*y + (y**2) * x2 -9))
-
+func = lambda x,y: x1 * (x ** 2) + x1x2 *x*y + (y**2) * x2
+C = [4,3,2,1]
+x = np.linspace(-2, 2, 50)
+y = np.linspace(-2, 2, 50)
+X, Y = np.meshgrid(x, y)
+plt.contour(X, Y, func(X, Y), levels=[i for i in C])
 plt.show()
+
